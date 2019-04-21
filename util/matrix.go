b5f7b8396339e78c34b9302a84f0df7e7f9d1f39
AnanaGame <gameanana@gmail.com>
2019-04-21 21:08:34 +0200

    Add new methods to Matrix

--- util/matrix.go
+++ util/matrix.go
@@ -2,8 +2,8 @@ package util
 
 import "gonum.org/v1/gonum/mat"
 
-// Returns the value of the dot product between *m* and *n*.
-// Row number of *m* must be equal to columns number of *n*.
+// Returns the value of the dot product between m and n.
+// Row number of m must be equal to columns number of n.
 func DotProduct(m, n mat.Matrix) mat.Matrix {
 	rows, _ := m.Dims()
 	_, columns := n.Dims()
@@ -13,8 +13,8 @@ func DotProduct(m, n mat.Matrix) mat.Matrix {
 	return result
 }
 
-// Return the value of a *fn* function (which takes as parameters the position row,
-// column and his value) applied to a matrix *m*.
+// Return the value of a function (which takes as parameters the position row,
+// column and his value) applied to a matrix m.
 func ApplyFunction(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matrix {
 	rows, columns := m.Dims()
 
@@ -23,7 +23,7 @@ func ApplyFunction(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matri
 	return result
 }
 
-// Returns the value of *m* mutliplied by *s* which is a float64.
+// Returns the value of m mutliplied by s which is a float64.
 func Scale(s float64, m mat.Matrix) mat.Matrix {
 	rows, columns := m.Dims()
 
@@ -32,12 +32,40 @@ func Scale(s float64, m mat.Matrix) mat.Matrix {
 	return result
 }
 
-// Returns the value of *m* multiplied by *n*.
-// *m* and *n* must have the same dimension.
+// Returns the value of m multiplied by n.
+// m and n must have the same dimension.
 func Multiply(m, n mat.Matrix) mat.Matrix {
 	rows, columns := m.Dims()
 
 	result := mat.NewDense(rows, columns, nil)
 	result.MulElem(m, n)
+	return result
+}
+
+// Returns the value of the sum between m and n
+func Sum(m, n mat.Matrix) mat.Matrix {
+	rows, columns := m.Dims()
+
+	result := mat.NewDense(rows, columns, nil)
+	result.Add(m, n)
+	return result
+}
+
+func AddScalar(i float64, m mat.Matrix) mat.Matrix {
+	rows, columns := m.Dims()
+
+	a := make([]float64, rows * columns)
+	for x := 0; x < rows * columns; x++ {
+		a[x] = i
+	}
+	n := mat.NewDense(rows, columns, a)
+	return Sum(m, n)
+}
+
+func Subtract(m, n mat.Matrix) mat.Matrix {
+	rows, columns := m.Dims()
+	
+	result := mat.NewDense(rows, columns, nil)
+	result.Sub(m, n)
 	return result
 }
